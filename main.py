203411f19cfab1000b038150e6c2886db8e16791
Alexey Lazarev <52629691+AlexeyLz@users.noreply.github.com>
2022-10-03 21:25:57 +0300

    adding webhook

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 import os
 from bot_connection import dp
 import bot_commands
+from aiogram import types
 from aiogram.utils import executor
+import logging
+from bot_connection import TOKEN, bot
+APP_NAME = os.getenv('APP_NAME')
+from aiogram.utils.executor import start_webhook
+
+WEBHOOK_HOST = 'https://tranlate-boy.onrender.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+# if __name__ == '__main__':
+#     executor.start_polling(dp, skip_updates=True)
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
