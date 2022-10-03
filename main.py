ced13bfb314764fefa4ae19fcdb388f9b1fad41b
Alexey Lazarev <52629691+AlexeyLz@users.noreply.github.com>
2022-10-03 21:47:43 +0300

    adding webhook

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'
+WEBAPP_HOST = '52.59.103.54'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 async def on_startup(dispatcher):
